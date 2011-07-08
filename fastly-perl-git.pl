9c5e4e3e0d451db7bda4ef2cd855cb55a05e3597
Artur Bergman <sky@crucially.net>
2011-07-07 20:17:01 -0700

    do this the right way and store them compressed

--- fastly-perl-git.pl
+++ fastly-perl-git.pl
@@ -5,23 +5,72 @@ use warnings;
 
 my $module = shift;
 
-my $output = `cpanm -L /tmp/foo/ --scandeps $module`;
+use JSON;
 
+use Data::Dumper;
+
+
+my $output = `cpanm -L /tmp/foo/ --scandeps $module --format json`;
+#my $output = `cat test.json`;
+my $data = from_json($output);
+
+#print Dumper($data);
+
+my $i = 0;
+sub process {
+    my $entry = shift;
+    my $dist = shift @$entry;
+    my $dep = shift @$entry;
+    
+    my $space = " " x $i;
+
+    $i++;
+    my @deps;
+    foreach my $list (@$dep) {
+	push @deps, "$list->[0]->{dist}/$list->[0]->{filename}";
+	process($list);
+    }
+    $i--;
+    next if -d "$dist->{dist}/$dist->{filename}";
+    unless(-d $dist->{dist}) {
+	system("mkdir $dist->{dist}");
+
+    }
+    system("cp $dist->{local_path} $dist->{dist}");
+    open(my $foo, ">$dist->{dist}/$dist->{distvname}.build") || die "$dist->{dist}/$dist->{distvname}.build";
+    print $foo join("\n", @deps);
+    close($foo);
+    system("git add $dist->{dist}");
+    system("git commit -m 'imported module $dist->{filename}'");
+    print "$space$dist->{module} => $dist->{filename} " . join(" ,", @deps) . "\n";
+}
+
+foreach my $list (@$data) {
+   process($list);
+    
+}
+
+print 
+exit;
 for my $item (split(/\s+/, $output)) {
-    next if $item =~/\\_/;
-    $item =~s/\s*[\_]+\s//;
-    my ($dir) = $item =~/(.*)-(.*?)/;
+
+    my ($author, $file) = split("/", $item);
+    print "$author, $file\n";
+
+    my ($dir) = $file =~/(.*)-(.*?)/;
+
+    print "$dir -- $file-\n";
     unless (-d $dir) {
 	system("mkdir $dir");
-	system("git add $dir");
+#	system("git add $dir");
     }
     next if -d "$dir/$item";
-    print "Installing $dir - $item\n";
+#    print "Installing $dir - $item\n";
     chdir($dir);
-    system("tar zxvf ~/.cpanm/latest-build/${item}.tar.gz");
-    system("git add $item");
+    system("cp ~/.cpanm/latest-build/${item}.tar.gz .");
+#    system("git add $item");
     chdir("..");
-    system("git commit -m 'added module $item' $dir");
+#    system("git commit -m 'added module $item' $dir");
 }
 
 
